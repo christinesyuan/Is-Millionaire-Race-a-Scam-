7f9d0d3ef777ae39accf474836502cefddf535ab
christinesyuan <145097330+christinesyuan@users.noreply.github.com>
2024-10-26 16:56:32 -0700

    Create millionaire race - obs and exp.r

---
+++ millionaire race - obs and exp.r
@@ -0,0 +1,1 @@
+final_table <- data.frame("Room Number" = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19), "Number of Failed Times" = c(0, 5, 6, 5, 7, 8, 3, 5, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0), "Expected Failed Times" = expected[-20], check.names = F)
